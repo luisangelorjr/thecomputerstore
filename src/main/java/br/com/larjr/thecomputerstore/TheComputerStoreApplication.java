66c49f4d23f2496c43a7b4e8cd8a4f6b73273dd0
Luis Angelo Rodrigues Jr <luisangelorjr@gmail.com>
2019-11-01 15:57:28 -0300

    First commit

---
+++ src/main/java/br/com/larjr/thecomputerstore/TheComputerStoreApplication.java
@@ -0,0 +1,12 @@
+package br.com.larjr.thecomputerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheComputerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheComputerStoreApplication.class, args);
+	}
+}
